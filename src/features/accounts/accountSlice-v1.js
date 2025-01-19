bbc3441b302ed24b01ee7fc009f913e5ec386f6f
2017kartik <kartikagarwal9119063420@gmail.com>
2025-01-19 10:02:47 +0530

    Implementing Redux Toolkit to Account Operation.

--- src/features/accounts/accountSlice-v1.js
+++ src/features/accounts/accountSlice-v1.js
@@ -1,41 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-const accountSlice = createSlice({
-  name: "account",
-  initialState,
-  reducer: {
-    deposit(state, action) {
-      state.balance += action.payload;
-    },
-    withdraw(state, action) {
-      state.balance -= action.payload;
-    },
-    requestLoan(state, action) {
-      if (state.loan > 0) return;
-      state.balance += action.payload.amount;
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-    },
-    payLoan(state) {
-      state.balance -= state.loan;
-      state.loan = 0;
-      state.loanPurpose = "";
-    },
-  },
-});
-
-console.log(accountSlice);
-
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
-export default accountSlice.reducer;  
-/*
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
@@ -105,4 +74,3 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: "account/payLoan" };
 }
-*/

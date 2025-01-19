2be62dab2dcc494796841c910b1719f760bb57e4
2017kartik <kartikagarwal9119063420@gmail.com>
2025-01-19 14:31:25 +0530

    Finalizing the project.

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -10,31 +10,54 @@ const initialState = {
 const accountSlice = createSlice({
   name: "account",
   initialState,
-  reducer: {
+  reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan > 0) return;
-      state.balance += action.payload.amount;
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.balance += action.payload.amount;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+      },
     },
     payLoan(state) {
       state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = "";
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-console.log(accountSlice);
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
-export default accountSlice.reducer;  
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
+export default accountSlice.reducer;
 /*
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
